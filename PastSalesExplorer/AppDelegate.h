53fd9602c75467357dfe182f917f91b4af3289db
Jordi Dosne <jordi@dosne.net>
2017-11-24 00:02:15 +0100

    Initial Commit

---
+++ PastSalesExplorer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PastSalesExplorer
+//
+//  Created by Jordi Dosne on 24/11/2017.
+//  Copyright © 2017 Jordi Dosne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PastSalesExplorer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PastSalesExplorer
+//
+//  Created by Jordi Dosne on 24/11/2017.
+//  Copyright © 2017 Jordi Dosne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
